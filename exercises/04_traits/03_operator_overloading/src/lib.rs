c725838b89ecb3d3d828bc27145bf3be74151a6c
Tan Chee Keong <tanck@sigmaprime.io>
2025-05-14 21:28:25 +0800

    chap4sec03

--- exercises/04_traits/03_operator_overloading/src/lib.rs
+++ exercises/04_traits/03_operator_overloading/src/lib.rs
@@ -8,7 +8,16 @@ struct Ticket {
 
 // TODO: Implement the `PartialEq` trait for `Ticket`.
 
-impl PartialEq for Ticket {}
+// By default, we can't compare Ticket with Ticket
+// But when we implement PartialEq for Ticket, we can now compare Ticket with Ticket
+// Because implementing PartialEq allows us to call the .eq() method
+impl PartialEq for Ticket {
+    fn eq(&self, other: &Self) -> bool {
+        self.title == other.title
+            && self.description == other.description
+            && self.status == other.status
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -29,6 +38,12 @@ mod tests {
             description: description.to_string(),
             status: status.to_string(),
         };
+        // when we call ticket1 == ticket2, it is translated to: ticket1.eq(&ticket2)
+        // Ref: https://rust-exercises.com/100-exercises/04_traits/03_operator_overloading.html#operators-are-traits
+        // therefore, it's using .eq() implicitly
+        // So, in ticket1.eq(&ticket2), ticket1 is the "&self" in eq(&self, other: &Self)
+        // the &Self in "other: &Self" is the type (just like any function input/arguments where we need to specify the type)
+        // the type is Ticket, so if we use "other: &Ticket" it also works
         assert!(ticket1 == ticket2);
     }
 
