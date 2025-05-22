1e0ac324b6ebbe1cf94fe4ca323ec4e3d15e1bc7
Tan Chee Keong <tanck@sigmaprime.io>
2025-05-22 18:23:12 +0800

    chap5sec13

--- exercises/05_ticket_v2/13_try_from/src/lib.rs
+++ exercises/05_ticket_v2/13_try_from/src/lib.rs
@@ -8,6 +8,46 @@ enum Status {
     Done,
 }
 
+// The type T is String, so I put <String> here:
+// Ref: https://rust-exercises.com/100-exercises/05_ticket_v2/13_try_from#tryfrom-and-tryinto-1
+impl TryFrom<String> for Status {
+    // we put the Error type as String
+    type Error = String;
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        let value_lowercase = value.to_lowercase(); //Note: to_lowercase() returns a String
+                                                    // the pattern on the LHS of match "ToDo" is &str, so need to convert
+        match value_lowercase.as_str() {
+            // after convert value to lowercase, the pattern should all be lowercase so that they can match
+            "todo" => Ok(Status::ToDo),
+            "inprogress" => Ok(Status::InProgress),
+            "done" => Ok(Status::Done),
+            _ => Err(format!("Invalid status: {}", value)),
+        }
+    }
+}
+
+// The following, from the solution given, is simpler
+// It uses the fact that since we implement TryFrom<&str>, so we get try_into() for free, so we use that
+// impl TryFrom<String> for Status {
+//     type Error = String;
+//     fn try_from(value: String) -> Result<Self, Self::Error> {
+//         value.as_str().try_into()
+//     }
+// }
+
+impl TryFrom<&str> for Status {
+    type Error = String;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let value_lowercase = value.to_lowercase();
+        match value_lowercase.as_str() {
+            "todo" => Ok(Status::ToDo),
+            "inprogress" => Ok(Status::InProgress),
+            "done" => Ok(Status::Done),
+            _ => Err(format!("Invalid status: {}", value)),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
