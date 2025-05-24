ab65015188969775eb540824ba944abedad29db2
Tan Chee Keong <tanck@sigmaprime.io>
2025-05-24 20:30:21 +0800

    chap6sec01

--- exercises/06_ticket_management/01_arrays/src/lib.rs
+++ exercises/06_ticket_management/01_arrays/src/lib.rs
@@ -1,7 +1,7 @@
 // TODO: Flesh out the `WeekTemperatures` struct and its method implementations to pass the tests.
 
 pub struct WeekTemperatures {
-    // TODO
+    temperature: [Option<i32>; 7],
 }
 
 pub enum Weekday {
@@ -14,17 +14,38 @@ pub enum Weekday {
     Sunday,
 }
 
+fn weekday_to_index(day: Weekday) -> usize {
+    match day {
+        Weekday::Monday => 0,
+        Weekday::Tuesday => 1,
+        Weekday::Wednesday => 2,
+        Weekday::Thursday => 3,
+        Weekday::Friday => 4,
+        Weekday::Saturday => 5,
+        Weekday::Sunday => 6,
+    }
+}
+
 impl WeekTemperatures {
     pub fn new() -> Self {
-        todo!()
+        // from the test, we see: assert_eq!(week_temperatures.get_temperature(Weekday::Monday), None);
+        // so before any temperature is set, the temperature is None
+        // therefore, we put the temperature as Option <i32>, not just i32
+        WeekTemperatures {
+            temperature: [None; 7],
+        }
     }
 
     pub fn get_temperature(&self, day: Weekday) -> Option<i32> {
-        todo!()
+        self.temperature[weekday_to_index(day)]
     }
 
     pub fn set_temperature(&mut self, day: Weekday, temperature: i32) {
-        todo!()
+        let index = weekday_to_index(day);
+        // temperature is an array with 7 elements, corresponding to each day of the week
+        // so self.temperature is an array, with elements either Option<i32> or None
+        // when we set the temperature, we want to set it for a particular day (the index)
+        self.temperature[index] = Some(temperature);
     }
 }
 
