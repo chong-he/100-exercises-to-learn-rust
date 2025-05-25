03b54006597c8278f2a203abbc5112b2adbed981
Tan Chee Keong <tanck@sigmaprime.io>
2025-05-25 11:42:51 +0800

    chap6sec04

--- exercises/06_ticket_management/04_iterators/src/lib.rs
+++ exercises/06_ticket_management/04_iterators/src/lib.rs
@@ -13,6 +13,25 @@ pub struct TicketStore {
     tickets: Vec<Ticket>,
 }
 
+// Implement IntoIterator for the TicketStore struct
+impl IntoIterator for TicketStore {
+    // Item is the thing to be iterated, e.g., in "for i in array", i is the Item
+    // So, here, we iterate over each Ticket in the Vec<Ticket>
+    type Item = Ticket;
+    // The below type IntoIter is about: which kind of iterator are we turning into
+    // https://doc.rust-lang.org/std/vec/struct.Vec.html#impl-IntoIterator-for-Vec%3CT,+A%3E
+    // this is about when we call .into_iter(), what type we get back? A: we get back std::vec::IntoIter<Ticket>
+    // e.g., when we do: let store = TicketStore::new();
+    // let iterator = store.into_iter(); --> this iterator is of type std::vec::IntoIter<Ticket>
+    // for ticket in iterator {} --> the iterator is as above, is the type that allows iteration
+    // and ticket is the individual Ticket that we iterate through
+    type IntoIter = std::vec::IntoIter<Ticket>;
+    fn into_iter(self) -> Self::IntoIter {
+        // self is TicketStore
+        // so, self.tickets is Vec<Ticket>
+        self.tickets.into_iter()
+    }
+}
 #[derive(Clone, Debug, PartialEq)]
 pub struct Ticket {
     pub title: TicketTitle,
