e8228bd786dae4e3b506671eab67371090d93db8
Tan Chee Keong <tanck@sigmaprime.io>
2025-05-29 22:11:20 +0800

    chap7sec04

--- exercises/07_threads/04_scoped_threads/src/lib.rs
+++ exercises/07_threads/04_scoped_threads/src/lib.rs
@@ -2,8 +2,29 @@
 //  and compute the sum of each half in a separate thread.
 //  Don't perform any heap allocation. Don't leak any memory.
 
+use std::thread;
+
 pub fn sum(v: Vec<i32>) -> i32 {
-    todo!()
+    let midpoint = v.len() / 2;
+
+    thread::scope(|scope| {
+        // sum1 is a JoinHandle (not the actual sum of the elements of the vector)
+        // this only runs the background to compute the sum
+        let sum1 = scope.spawn(|| {
+            let v1 = &v[0..midpoint];
+            v1.iter().sum()
+        });
+        // need to call .join() to become sum
+        // is like calling .join() to "redeem" the actual result, which is the sum
+        let t1: i32 = sum1.join().unwrap();
+
+        let sum2 = scope.spawn(|| {
+            let v2 = &v[midpoint..];
+            v2.iter().sum()
+        });
+        let t2: i32 = sum2.join().unwrap();
+        t1 + t2
+    })
 }
 
 #[cfg(test)]
